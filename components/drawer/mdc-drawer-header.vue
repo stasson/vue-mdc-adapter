e1fd4a45754eb7b2ecd223386b1bc9a44c8d0e2c
Peter Lauck <griest024@gmail.com>
2017-06-04 19:36:25 -0400

    implement all drawer components fixes #15

---
+++ components/drawer/mdc-drawer-header.vue
@@ -0,0 +1,15 @@
+
+<template>
+  <header :class="'mdc-' + this.type.toString() + '-drawer__header'">
+    <div :class="'mdc-' + this.type.toString() + '-drawer__header-content'">
+      <slot />
+    </div>
+  </header>
+</template>
+
+<script lang="babel">
+export default {
+  name: 'mdc-drawer-header',
+  props: ['type']
+}
+</script>
---
+++ components/drawer/mdc-drawer-toolbar-spacer.vue
@@ -0,0 +1,13 @@
+
+<template>
+  <div :class="'mdc-' + this.type.toString() + '-drawer__toolbar-spacer'">
+    <slot />
+  </div>
+</template>
+
+<script lang="babel">
+export default {
+  name: 'mdc-drawer-toolbar-spacer',
+  props: ['type']
+}
+</script>
