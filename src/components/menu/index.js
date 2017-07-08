3c4608aa436193f392e6c1d6999aa26166025bef
Sebastien Tasson <sebastien.tasson@gmail.com>
2017-07-08 12:40:36 +0000

    refactor menu/index.js

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,8 +1,20 @@
+import VueMDCMenu from './mdc-menu'
+import VueMDCMenuItem from './mdc-menu-item'
+import VueMDCMenuDivider from './mdc-menu-divider'
+import VueMDCMenuAnchor from './mdc-menu-anchor'
+
+export {
+  VueMDCMenu,
+  VueMDCMenuItem,
+  VueMDCMenuDivider,
+  VueMDCMenuAnchor
+}
+
 export default {
   install (vm) {
-    vm.component('mdc-menu', require('./mdc-menu'))
-    vm.component('mdc-menu-item', require('./mdc-menu-item'))
-    vm.component('mdc-menu-divider', require('./mdc-menu-divider'))
-    vm.component('mdc-menu-anchor', require('./mdc-menu-anchor'))
+    vm.component('mdc-menu', VueMDCMenu)
+    vm.component('mdc-menu-item', VueMDCMenuItem)
+    vm.component('mdc-menu-divider', VueMDCMenuDivider)
+    vm.component('mdc-menu-anchor', VueMDCMenuAnchor)
   }
 }
