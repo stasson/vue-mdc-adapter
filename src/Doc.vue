95fcbff266eb161c7ea34b594e68818236796336
Sebastien Tasson <sebastien.tasson@gmail.com>
2017-09-30 07:52:37 +0000

    feat(button): add style for mdc-icon

--- src/Doc.vue
+++ src/Doc.vue
@@ -41,6 +41,7 @@
             <mdc-button >baseline</mdc-button>
             <mdc-button compact>compact</mdc-button>
             <mdc-button dense>dense</mdc-button>
+            <mdc-button><mdc-icon icon="favorite"></mdc-icon>icon</mdc-button>
             <mdc-button disabled>disabled</mdc-button>
           </div>
           <mdc-subheading>Raised Buttons</mdc-subheading>
@@ -48,6 +49,7 @@
             <mdc-button raised>baseline</mdc-button>
             <mdc-button raised compact>compact</mdc-button>
             <mdc-button raised dense>dense</mdc-button>
+            <mdc-button raised><mdc-icon icon="favorite"></mdc-icon>icon</mdc-button>
             <mdc-button raised disabled>disabled</mdc-button>
           </div>
 
@@ -56,6 +58,7 @@
             <mdc-button unelevated>baseline</mdc-button>
             <mdc-button unelevated compact>compact</mdc-button>
             <mdc-button unelevated dense>dense</mdc-button>
+            <mdc-button unelevated ><mdc-icon icon="favorite"></mdc-icon>icon</mdc-button>
             <mdc-button unelevated disabled>disabled</mdc-button>
           </div>
 
@@ -64,6 +67,7 @@
             <mdc-button stroked>baseline</mdc-button>
             <mdc-button stroked compact>compact</mdc-button>
             <mdc-button stroked dense>dense</mdc-button>
+            <mdc-button stroked><mdc-icon icon="favorite"></mdc-icon>icon</mdc-button>
             <mdc-button stroked disabled>disabled</mdc-button>
           </div>
 
