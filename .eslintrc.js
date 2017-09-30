f0e3fda76e263d6196486f7ae3c8ba11b50862e8
Sebastien Tasson <sebastien.tasson@gmail.com>
2017-09-30 07:32:13 +0000

    build(eslint): allow console and debug for dev builds

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,8 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 1,
+    // allow debugger during development
+    'no-console': process.env.NODE_ENV === 'production' ? 2 : 1
   }
 }
