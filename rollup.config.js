82655ff458cd6d9bad68f34145e8ea6df278c017
Sebastien Tasson <sebastien.tasson@gmail.com>
2017-10-01 06:44:02 +0000

    build(rollup): refactor rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
-import vue from 'rollup-plugin-vue2'
+import vue from 'rollup-plugin-vue'
 import uglify from 'rollup-plugin-uglify'
 import sass from 'rollup-plugin-sass';
 import filesize from 'rollup-plugin-filesize'
@@ -13,69 +13,80 @@ import { minify } from 'uglify-es'
 const isProduction = process.env.NODE_ENV === `production`
 const isDevelopment = process.env.NODE_ENV === `development`
 
+const libPath = (isDevelopment 
+      ? 'dist/vue-mdc-adapter.js' 
+      : 'dist/vue-mdc-adapter.min.js')
+
+const babelConfig = {
+  'compact': false,
+  'babelrc': false,
+  'presets': [ 
+    [
+      // env preset https://github.com/babel/babel-preset-env
+      'env', 
+      // let rollup take care of modules 
+      { 'modules': false } 
+    ]
+  ],
+  'plugins': [
+    // let rollup bundle helpers once
+    // see https://github.com/rollup/rollup-plugin-babel#helpers
+    'external-helpers'  
+  ]
+}
+
+  
+const sassConfig = {
+  include: [ '**/*.css', '**/*.scss' ],
+  options: {includePaths: ['node_modules']},
+  processor: css => postcss((isDevelopment
+                      ? [autoprefixer()]
+                      : [autoprefixer(), csso()]))
+                    .process(css)
+                    .then(result => result.css)
+}
+
+if (isProduction) {
+  sassConfig.output = `dist/vue-mdc-adapter.min.css`
+} else {
+  sassConfig.insert = true
+}
+
 const config = {
   input: 'components/entry.js',
   output: {
-    file: (isDevelopment 
-      ? 'dist/vue-mdc-adapter.js' 
-      : 'dist/vue-mdc-adapter.min.js'),
+    file: libPath,
     format: 'umd',
     name: 'VueMDCAdapter'
   },
   external: ['vue'],
   plugins: [
-    vue (),
+    vue ({ autoStyles: false, styleToImports: true }),
     resolve({ jsnext: true, main: true, browser: true }),
-    sass({
-      include: [ '**/*.css', '**/*.scss' ],
-      options: {includePaths: ['node_modules']},
-      output: (isDevelopment 
-      ? 'dist/vue-mdc-adapter.css' 
-      : 'dist/vue-mdc-adapter.min.css'),
-      processor: css => postcss((isDevelopment
-                          ? [autoprefixer()]
-                          : [autoprefixer(), csso()]))
-                        .process(css)
-                        .then(result => result.css)
-    }),
+    sass(sassConfig),
+    babel(babelConfig),
     commonjs (),
-    ... ( isProduction
-          ? [ uglify({}, minify), filesize() ] 
-          : [] ),
-    babel({
-      presets: [ 
-        [
-          'es2015', 
-          {modules: false} 
-        ]
-      ],
-      babelrc: false,
-      plugins: [
-        'external-helpers'
-      ]
-    }),
   ],
-  sourcemap: isDevelopment,
-  onwarn (warning) {
-    // skip certain warnings
-    if (warning.code == 'NON_EXISTENT_EXPORT' 
-      && warning.name == 'MDCIconToggleAdapter') {
-        return;
-    } else {
-      // console.warn everything else
-      console.log(warning);
-      console.warn(warning.message);
-    }
-  }
+  sourcemap: isDevelopment ? 'inline' : true,
+  onwarn
 }
 
-if (isDevelopment) {
-  // config.plugins.push(livereload())
-  // config.plugins.push(serve({
-  //   contentBase: './public/',
-  //   port: 8080,
-  //   open: true
-  // }))
+if (isProduction) {
+  config.plugins.push(uglify({}, minify))
+  config.plugins.push(filesize())
 }
 
 export default config
+
+
+function onwarn (warning) {
+
+  // skip certain warnings
+  if (warning.code == 'NON_EXISTENT_EXPORT') {
+    if (warning.name == 'MDCIconToggleAdapter') return;
+  } 
+
+  // console.warn everything else
+  console.log(warning);
+  console.warn(warning.message);
+}
