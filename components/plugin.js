1f22875259343aebc04c9fe2bb8dc5d201d3e97d
stasson <sebastien.tasson@gmail.com>
2017-11-27 18:28:47 +0100

    refactor: simplify plugin components registration

--- components/plugin.js
+++ components/plugin.js
@@ -1,15 +1,11 @@
 export default function (components) { 
-
-  let map = Object.values(components).reduce( (result, component) => {
-    return Object.assign(result, { [component.name]: component })
-  }, {})
-
   return {
     install: (vm) => {
-      for (let key in map) {
-        vm.component(key, map[key])
+      for (let key in components) {
+        let component = components[key]
+          vm.component(component.name,component)
       }
     },
-    components: map
+    components
   } 
 }
