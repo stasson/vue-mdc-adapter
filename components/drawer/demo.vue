1662382a18ef28347dead825e6ee7a89e9cc80a3
stasson <sebastien.tasson@gmail.com>
2017-11-28 16:32:33 +0100

    fix(demo): set iframe elevation to 1

--- components/drawer/demo.vue
+++ components/drawer/demo.vue
@@ -1,5 +1,5 @@
 <template>
-<iframe class="mdc-demo mdc-demo--fluid" scrolling='no' title='vue-mdc-adapter App Layout' 
+<iframe class="mdc-demo mdc-elevation--z1" scrolling='no' title='vue-mdc-adapter App Layout' 
   :src="iframe" width="400" height="250" frameborder='no' 
   allowtransparency='false' allowfullscreen='false'>
 </iframe>
