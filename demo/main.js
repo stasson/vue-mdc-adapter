ca64853baae4ce501712128ac220f3d4ace2decf
stasson <sebastien.tasson@gmail.com>
2017-11-30 19:19:25 +0100

    feat(demo): add vue-analytics

--- demo/main.js
+++ demo/main.js
@@ -1,6 +1,7 @@
 import './styles/demo.scss'
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueAnalytics from 'vue-analytics'
 import VueMDCAdapter from 'vue-mdc-adapter'
 import index from './index.vue'
 import routes from './routes.js'
@@ -8,12 +9,16 @@ import routes from './routes.js'
 Vue.config.productionTip = true
 Vue.use(VueRouter)
 Vue.use(VueMDCAdapter)
-
 const router = new VueRouter({
   routes,
   scrollBehavior () {
     return { x: 0, y: 0 }
   }
+})
+
+Vue.use(VueAnalytics, {
+  id: 'UA-110490450-1',
+  router
 })
 
 // mount app
