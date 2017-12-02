d420fe6c043eb7d815629eb1207194453f3ffc64
stasson <sebastien.tasson@gmail.com>
2017-12-02 17:33:53 +0100

    build(dist): simplify webpack config as per new ESM distribution
    BREAKING CHANGE: UMD distribution names have changed. see docs/getting-started.md

--- components/entry.esm.js
+++
@@ -1,4 +0,0 @@
-import './entry.esm.scss'
-import plugin from './index.js'
-export * from './index.js'
-export default plugin
--- components/list/entry.esm.js
+++
@@ -1,4 +0,0 @@
-import './styles.scss'
-import plugin from './index.js'
-export * from './index.js'
-export default plugin
