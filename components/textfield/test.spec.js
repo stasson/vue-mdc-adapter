6e23ba25d5207554c304b05ba2b46409c26993ce
stasson <sebastien.tasson@gmail.com>
2018-01-13 17:55:58 +0100

    test(textfield): add new foundation classes sanity checks

--- components/textfield/test.spec.js
+++ components/textfield/test.spec.js
@@ -7,8 +7,13 @@ pluginSanityCheck(__dirname, plugin)
 describe ('mdcTextField', () => {
     const wrapper = mount(mdcTextField, {
         propsData: {
+            label: 'label',
             helptext:"helptext"
         }
+    })
+
+    describe('labelFoundation', () => {
+        checkValidFoundation(wrapper.vm.labelFoundation)
     })
 
     describe('bottomLineFoundation', () => {
@@ -18,4 +23,30 @@ describe ('mdcTextField', () => {
     describe('helperTextFoundation', () => {
         checkValidFoundation(wrapper.vm.helperTextFoundation)
     })
+})
+
+describe ('mdcTextField', () => {
+    const wrapper = mount(mdcTextField, {
+        propsData: {
+            outline: true
+        }
+    })
+
+    describe('outlineFoundation', () => {
+        checkValidFoundation(wrapper.vm.outlineFoundation)
+    })
+
+})
+
+describe ('mdcTextField', () => {
+    const wrapper = mount(mdcTextField, {
+        propsData: {
+            trailingIcon: 'event'
+        }
+    })
+
+    describe('iconFoundation', () => {
+        checkValidFoundation(wrapper.vm.iconFoundation)
+    })
+
 })
