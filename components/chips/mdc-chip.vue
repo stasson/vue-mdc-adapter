d8fd575d2c51a17b947f9671cdf68b59aa96c1e0
philip ross <philip@satchel.scot>
2018-03-08 16:49:38 +0000

    feat(chips): Implement filter and choice chip-set variants. Implement trailing icon interaction handler.

--- components/chips/mdc-chip.vue
+++ components/chips/mdc-chip.vue
@@ -1,9 +1,9 @@
 <template>
-<div class="mdc-chip" :class="classes" :style="styles" tabindex="0" >
+<div :class="classes" :style="styles" tabindex="0" >
   <i ref="leadingIcon" class="mdc-chip__icon mdc-chip__icon--leading" 
     :class="leadingClasses" v-if="haveleadingIcon"
   >{{leadingIcon}}</i>
-  <div classes="mdc-chip__text">
+  <div class="mdc-chip__text">
     <slot></slot>
   </div>
   <i ref="trailingIcon" class="mdc-chip__icon mdc-chip__icon--trailing" tabindex="0" role="button" 
@@ -14,7 +14,7 @@
 
 <script>
 import MDCChipFoundation from '@material/chips/chip/foundation';
-import { CustomLinkMixin, DispatchEventMixin } from '../base';
+import { CustomLinkMixin, DispatchEventMixin, emitCustomEvent } from '../base';
 import { RippleBase } from '../ripple';
 
 export default {
@@ -28,25 +28,47 @@ export default {
   },
   data() {
     return {
-      classes: {},
+      classes: {
+        'mdc-chip': true,
+      },
       styles: {},
     };
   },
-  methods: {},
+  methods: {
+    toggleSelected() {
+      this.foundation.toggleSelected();
+    },
+  },
   mounted() {
     this.foundation = new MDCChipFoundation({
       addClass: className => this.$set(this.classes, className, true),
-      removeClass: className => this.$delete(this.rootClasses, className),
-      hasClass: className => this.$refs.root.classList.contains(className),
+      removeClass: className => this.$delete(this.classes, className),
+      hasClass: className => this.$el.classList.contains(className),
       registerInteractionHandler: (type, handler) =>
         this.$el.addEventListener(type, handler),
       deregisterInteractionHandler: (type, handler) =>
         this.$el.removeEventListener(type, handler),
       notifyInteraction: () => {
         this.dispatchEvent({ type: 'click' });
+        emitCustomEvent(
+          this.$el,
+          MDCChipFoundation.strings.INTERACTION_EVENT,
+          {
+            chip: this,
+          },
+          true,
+        );
       },
       notifyTrailingIconInteraction: () => {
         this.dispatchEvent({ type: 'trailingIconClick' });
+        emitCustomEvent(
+          this.$el,
+          MDCChipFoundation.strings.TRAILING_ICON_INTERACTION_EVENT,
+          {
+            chip: this,
+          },
+          true,
+        );
       },
       registerTrailingIconInteractionHandler: (evtType, handler) => {
         if (this.$refs.trailingIcon) {
