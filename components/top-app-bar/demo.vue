c6a46dd83d17944647bb184828dfb0722227460e
philip ross <philip@satchel.scot>
2018-03-09 17:07:11 +0000

    feat(top-app-bar): Implement mdc-top-app-bar as per MDC 0.32.0

---
+++ components/top-app-bar/demo.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="mdc-demo--appbar">
+  <section class="mdc-demo">
+    <mdc-top-app-bar title="Title2">
+        <mdc-top-app-bar--action event="show-help" icon="help"></mdc-top-app-bar--action>
+        <mdc-top-app-bar--action event="show-info" icon="info_outline"></mdc-top-app-bar--action>
+    </mdc-top-app-bar>
+  </section>
+
+</div>
+</template>
+
+<script>
+</script>
+
+<style>
+@import url('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+
+.mdc-demo--appbar {
+  width: 100%;
+}
+</style>
