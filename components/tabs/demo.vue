209de2fbaed682e52e00b5186f714b1258a5b1ca
philip ross <philip@satchel.scot>
2018-03-26 15:17:44 +0100

    fix(mdc-tab): show icons in tabs

--- components/tabs/demo.vue
+++ components/tabs/demo.vue
@@ -1,9 +1,20 @@
 <template>
-  <div class="mdc-demo mdc-demo--container">
+<div class="">
+  <h3>Simple</h3>
+  <section class="mdc-demo mdc-demo--container">
     <mdc-tab-bar indicator-accent>
-      <mdc-tab>item one</mdc-tab>              
-      <mdc-tab>item two</mdc-tab>              
-      <mdc-tab>item three</mdc-tab>              
+      <mdc-tab>item one</mdc-tab>
+      <mdc-tab>item two</mdc-tab>
+      <mdc-tab>item three</mdc-tab>
     </mdc-tab-bar>
-  </div>
+  </section>
+  <h3>With icons and text</h3>
+  <section class="mdc-demo mdc-demo--container">
+    <mdc-tab-bar indicator-accent>
+      <mdc-tab icon="phone">Recents</mdc-tab>
+      <mdc-tab icon="favorite">Favorites</mdc-tab>
+      <mdc-tab icon="personal_pin">Nearby</mdc-tab>
+    </mdc-tab-bar>
+  </section>
+</div>
 </template>
