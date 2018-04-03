755ffac920d245dd108cc2e9d19177482525292e
philip ross <philip@satchel.scot>
2018-04-03 18:25:06 +0100

    feat(mdc-select): Implement native select

--- components/select/demo.vue
+++ components/select/demo.vue
@@ -1,56 +1,42 @@
 <template>
-  <div class="mdc-demo mdc-demo--container">
-    <div>
-      <mdc-select v-model="selectedType" label="Pick up a food type">
-        <mdc-option v-for="type in types" :key="type"
-        >{{type}}</mdc-option>
-      </mdc-select>
-      <br><br><br>
-      <mdc-select box v-model="selectedValue" label="Pick up a food"
-        v-if="selectedType">
-        <mdc-option v-for="option of options" :key="option"
-        :value="option.toLowerCase()"
-        >{{option}}</mdc-option>
-      </mdc-select>
+<div class="mdc-demo mdc-demo--container">
+  <div>
+    <mdc-select v-model="selectedType" label="Pick up a food type">
+      <mdc-option v-for="type in types" :key="type">{{type}}</mdc-option>
+    </mdc-select>
+    <br><br><br>
+    <mdc-select box v-model="selectedValue" label="Pick up a food" v-if="selectedType">
+      <mdc-option v-for="option of options" :key="option" :value="option.toLowerCase()">{{option}}</mdc-option>
+    </mdc-select>
 
-      <mdc-caption tag="p" v-if="selectedType"
-      >Selected Value: {{ selectedValue }}</mdc-caption>
-      <br>
+    <mdc-caption tag="p" v-if="selectedType">Selected Value: {{ selectedValue }}</mdc-caption>
+    <br>
 
-      <mdc-select multiple v-model="selectedValues" label="Pick one or more"
-      v-if="selectedType">
-      <mdc-option v-for="option of options" :key="option"
-      :value="option.toLowerCase()">
-      {{option}}
-      </mdc-option>
-      </mdc-select>
-      <mdc-caption tag="p" v-if="selectedType"
-      >Multi Select: {{ selectedValues }}</mdc-caption>
-    </div>
   </div>
+</div>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        selectedType: undefined,
-        selectedValue: undefined,
-        selectedValues: [],
-        food: {
-          'Vegetables' : ['Spinach', 'Carrots', 'Onions', 'Broccoli'], 
-          'Meat' : [ 'Eggs', 'Chicken', 'Fish', 'Turkey', 'Pork' , 'Beef'], 
-          'Fruits' : ['Apples', 'Oranges', 'Bananas', 'Berries', 'Lemons'], 
-        }
-      }
+export default {
+  data() {
+    return {
+      selectedType: undefined,
+      selectedValue: undefined,
+      selectedValues: [],
+      food: {
+        Vegetables: ['Spinach', 'Carrots', 'Onions', 'Broccoli'],
+        Meat: ['Eggs', 'Chicken', 'Fish', 'Turkey', 'Pork', 'Beef'],
+        Fruits: ['Apples', 'Oranges', 'Bananas', 'Berries', 'Lemons'],
+      },
+    };
+  },
+  computed: {
+    types() {
+      return Object.keys(this.food);
     },
-    computed: {
-      types () {
-        return Object.keys(this.food)
-      }, 
-      options () {
-        return this.selectedType ? this.food[this.selectedType] : []
-      } 
-    }
-  }
+    options() {
+      return this.selectedType ? this.food[this.selectedType] : [];
+    },
+  },
+};
 </script>
---
+++ components/select/mdc-optgroup.vue
@@ -0,0 +1,16 @@
+<template>
+   <optgroup :disabled="disabled" :label="label"
+    class="mdc-optgroup">
+     <slot></slot>
+   </optgroup>
+</template>
+
+<script>
+export default {
+  name: 'mdc-optgroup',
+  props: {
+    label: String,
+    disabled: Boolean,
+  },
+};
+</script>
