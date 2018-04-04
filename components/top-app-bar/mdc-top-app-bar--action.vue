5ec8ce3e213ceec3d2caf9d3280e4280ca9c52b1
philip ross <philip@satchel.scot>
2018-04-04 17:57:37 +0100

    feat(mdc-top-app-bar): Support dense mode from material-components-web v0.34.1. Support underlying navigation event as wel as click.

--- components/top-app-bar/mdc-top-app-bar--action.vue
+++ components/top-app-bar/mdc-top-app-bar--action.vue
@@ -1,16 +1,15 @@
 <template>
-<a href="#" class="mdc-top-app-bar--action mdc-top-app-bar__action-item" :class="actioniconClasses" @click="dispatchEvent">
+<a href="#" class="mdc-top-app-bar--action mdc-top-app-bar__action-item" :class="actioniconClasses" v-on="$listeners">
   <slot>{{icon}}</slot>
 </a>
 </template>
 
 <script>
-import { DispatchEventMixin } from '../base';
 import { RippleMixin } from '../ripple';
 
 export default {
   name: 'mdc-top-app-bar--action',
-  mixins: [DispatchEventMixin, RippleMixin],
+  mixins: [RippleMixin],
   props: {
     icon: String,
     iconClasses: Object,
