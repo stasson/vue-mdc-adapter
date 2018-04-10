9ced1b41e99c7339803060947ff5f91e7b2f511b
philip ross <philip@satchel.scot>
2018-04-10 12:20:34 +0100

    feat(mdc-menu): Support v-model for open/close

--- components/menu/demo.vue
+++ components/menu/demo.vue
@@ -1,14 +1,24 @@
 <template>
-  <div class="mdc-demo mdc-demo--container">
-    <mdc-menu-anchor class="myAnchor">
-      <mdc-button raised @click="$refs.menu.show()">Open Menu</mdc-button>
-      <mdc-menu ref="menu">
-        <mdc-menu-item >A Menu Item</mdc-menu-item>
-        <mdc-menu-item >Another Menu Item</mdc-menu-item>
-        <mdc-menu-item disabled >Disabled Menu Item</mdc-menu-item>
-        <mdc-menu-divider>Another Menu Item</mdc-menu-divider>
-        <mdc-menu-item >Parted Menu Item</mdc-menu-item>
-      </mdc-menu>
-    </mdc-menu-anchor>
-  </div>
+<div class="mdc-demo mdc-demo--container">
+  <mdc-menu-anchor class="myAnchor">
+    <mdc-button raised @click="open=true">Open Menu</mdc-button>
+    <mdc-menu v-model="open">
+      <mdc-menu-item>A Menu Item</mdc-menu-item>
+      <mdc-menu-item>Another Menu Item</mdc-menu-item>
+      <mdc-menu-item disabled>Disabled Menu Item</mdc-menu-item>
+      <mdc-menu-divider>Another Menu Item</mdc-menu-divider>
+      <mdc-menu-item>Parted Menu Item</mdc-menu-item>
+    </mdc-menu>
+  </mdc-menu-anchor>
+</div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      open: false,
+    };
+  },
+};
+</script>
