f171c7a7cad091be06de8301b133f6cf61cd2536
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2018-06-21 05:33:33 +0300

    initial implementation of z index on elevation proper demo

--- components/elevation/mdc-elevation.vue
+++ components/elevation/mdc-elevation.vue
@@ -1,10 +1,29 @@
 <template>
-  <div class="mdc-elevation"/>
+  <div
+    :class="classes"
+    class="mdc-elevation">
+    <slot/>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'mdc-elevation',
-  props: {}
+  props: {
+    z: {
+      type: [Number, String],
+      default() {
+        return 1
+      }
+    }
+  },
+  data() {
+    let elevationClasses = {}
+    elevationClasses[`mdc-elevation--z${this.z}`] = true
+
+    return {
+      classes: elevationClasses
+    }
+  }
 }
 </script>
